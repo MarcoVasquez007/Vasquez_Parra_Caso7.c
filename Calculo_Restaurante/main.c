68b41a3f0f3799ca452346b12219cc335c55b1ed
jorgeparrajp <jorgeenriqueparrachora@gmail.com>
2026-06-27 15:30:58 -0500

    Terminado el switch con las opciones del menu Jorge Parra

--- Calculo_Restaurante/main.c
+++ Calculo_Restaurante/main.c
@@ -18,3 +18,32 @@ int main(void) {
         printf("6. Salir\n");
 
         opcion = leerEntero("Opcion: ", 1, 6);
+        switch (opcion) {
+            case 1:
+                menuIngredientes(&sistema);
+                break;
+            case 2:
+                menuPlatos(&sistema);
+                break;
+            case 3:
+                menuRelaciones(&sistema);
+                break;
+            case 4:
+                menuReportes(&sistema);
+                break;
+            case 5:
+                guardarDatos(&sistema);
+                printf("Cambios guardados.\n");
+                pausar();
+                break;
+            case 6:
+                if (confirmar("Desea guardar antes de salir")) {
+                    guardarDatos(&sistema);
+                }
+                printf("Programa finalizado.\n");
+                break;
+        }
+    } while (opcion != 6);
+
+    return 0;
+}
